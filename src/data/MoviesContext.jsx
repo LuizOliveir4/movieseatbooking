4b5e38b2afc658b642c14cc48e4e3fd8a6807771
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-01-24 05:43:32 +0100

    url: DataContext (GitHub)

--- src/data/MoviesContext.jsx
+++ src/data/MoviesContext.jsx
@@ -1,5 +1,5 @@
-const url = "http://localhost:3000/movies/"
-//const url = "https://luizoliveir4.github.io/movieseatbookingapi/movies.json?id=123"
+//const url = "http://localhost:3000/movies/"
+const url = "https://luizoliveir4.github.io/movieseatbookingapi/movies.json?id=123"
 
 export async function loadMovies() {
     const response = await fetch(url);

43cfa1ad8735d5838be6a57fd2b1fee7c9a907ea
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-01-29 11:03:07 +0100

    Insert div to show formik alerts

--- src/components/admin/AdminForm.jsx
+++ src/components/admin/AdminForm.jsx
@@ -36,11 +36,11 @@ export const AdminForm = ({ addMovie }) => {
         <h2>Enter a Movie:</h2>
         <form onSubmit={formik.handleSubmit}>
             <input id="title" name="title" type="text" placeholder="Title" onChange={formik.handleChange} value={formik.values.title}/>
-            {formik.errors.title ? <span className="error">{formik.errors.title}</span> : null}
+            <div> {formik.errors.title ? <span className="error">{formik.errors.title}</span> : null} </div>
             <input id="year" name="year" type="text" placeholder="Year" onChange={formik.handleChange} value={formik.values.year}/>
-            {formik.errors.year ? <span className="error">{formik.errors.year}</span> : null}
+            <div> {formik.errors.year ? <span className="error">{formik.errors.year}</span> : null} </div>
             <input id="price" name="price" type="text" placeholder="Price" onChange={formik.handleChange} value={formik.values.price}/>
-            {formik.errors.price ? <span className="error">{formik.errors.price}</span> : null}
+            <div> {formik.errors.price ? <span className="error">{formik.errors.price}</span> : null} </div>
             <button type="submit" >ADD</button>
         </form>
     </div>
